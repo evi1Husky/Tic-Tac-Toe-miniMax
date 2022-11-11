cc58cda1d3aec50259fb03e62d944ad3d0adb0d6
evi1Husky <evi1husky0@gmail.com>
2022-11-11 16:59:46 +1000

    add active css pseudoclass

--- script.js
+++ script.js
@@ -4,24 +4,25 @@ const gameBoard = (() => {
 })();
 
 const renderer = (() => {
-  const getBoardsquares = () => document.querySelectorAll(".xo");
-  const boardSquares = getBoardsquares();
+  const getBoardSquares = () => document.querySelectorAll(".xo");
+  const boardSquares = getBoardSquares();
   const updateBoard = () => {
     for (let i = 0; i < 9; i++) {
-      boardSquares[i].innerHTML = gameBoard.xoArray[i]
+      boardSquares[i].innerHTML = gameBoard.xoArray[i];
       if (boardSquares[i].innerHTML === player.symbol) {
         boardSquares[i].style.color = player.color;
       } else if (boardSquares[i].innerHTML === computer.symbol) {
         boardSquares[i].style.color = computer.color;
-      }
-    }
+      };
+    };
     updateGameStats();
+    console.log(gameBoard.xoArray);
   };
   const getStats = () => document.querySelector(".stats");
   const gameStats = getStats();
   const updateGameStats = () => {
     gameStats.innerHTML = `player:&nbsp;&nbsp;<span>${player.score}</span> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;computer:&nbsp;&nbsp;<span>${computer.score}</span></p>`
-  }
+  };
   return {boardSquares, updateBoard};
 })();
 
@@ -37,16 +38,25 @@ const computer = (() => {
   let symbol = "o";
   const color = "#ff8383";
   let difficulty = "easy";
-  const computerMove = () => {
+  const easyMove = () => {
     const availableSquares = [];
     for (let [index, xo] of gameBoard.xoArray.entries()) {
       if (xo === "") {
         availableSquares.push(index);
-      }
-    }
+      };
+    };
     const computerChoice = availableSquares[Math.random() * availableSquares.length | 0];
     gameBoard.xoArray[computerChoice] = computer.symbol;
   }
+  const computerMove = () => {
+    if (difficulty === "easy") {
+      easyMove();
+    } else if (difficulty === "medium") {
+      alert("medium move");
+    } else if (difficulty === "unbeatable") {
+      alert("unbeatable move");
+    };
+  };
   return {symbol, color, score, difficulty, computerMove};
 })();
 
@@ -57,15 +67,14 @@ const game = (() => {
         if (gameBoard.xoArray[i] === "") {
           gameBoard.xoArray[i] = player.symbol;
           renderer.updateBoard();
+          setTimeout(() => {
+            computer.computerMove();
+            renderer.updateBoard();
+          }, "300");
         } else {
           return;
         };
-        setTimeout(() => {
-          computer.computerMove();
-          renderer.updateBoard();
-        }, "300");
-        console.log(gameBoard.xoArray);
-      })}};
+    })}};
     return {loop};
 })();
 
